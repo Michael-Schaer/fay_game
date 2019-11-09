5d48668ede459a9e99fb8df8506e361bf1bd0bfc
Michael-Schaer <mdschaer@gmail.com>
2019-11-09 16:54:54 +0100

    Crafting Scene

---
+++ Assets/Scripts/CutManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Fay.Camping
+{
+    public class CutManager : MonoBehaviour
+    {
+        [SerializeField] Image laser, position_neutral, position_cut, position_hammeron;
+        [SerializeField] Image stone_1, stone_2;
+        [SerializeField] Button button1, button2, button3;
+
+        private int imageCounter = 0;
+
+        int currentCycle;
+
+        public void Initialize()
+        {
+            ShowButton();
+        }
+
+        public void SwitchImage()
+        {
+            if (imageCounter == 0)
+            {
+                laser.enabled = true;
+            }
+            else if (imageCounter == 1)
+            {
+                laser.enabled = false;
+                stone_1.enabled = false;
+                stone_2.enabled = true;
+                button1.gameObject.SetActive(false);
+                button2.gameObject.SetActive(true);
+            }
+            else if (imageCounter == 2)
+            {
+                position_neutral.enabled = false;
+                position_cut.enabled = true;
+                button2.gameObject.SetActive(false);
+                button3.gameObject.SetActive(true);
+            }
+            else if (imageCounter == 3)
+            {
+                position_cut.enabled = false;
+                position_hammeron.enabled = true;
+                button3.gameObject.SetActive(false);
+                button1.gameObject.SetActive(true);
+            }
+            else
+            {
+                Finish();
+            }
+            imageCounter++;
+
+        }
+
+        void ShowButton()
+        {
+            button1.gameObject.SetActive(true);
+        }
+
+        void Finish()
+        {
+            button1.gameObject.SetActive(false);
+            MemoryManager.Instance.Next();
+        }
+    }
+}
---
+++ Assets/Scripts/RingManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Fay.Camping
+{
+    public class RingManager : MonoBehaviour
+    {
+        [SerializeField] Image first_1, first_2, second_1, second_2;
+        [SerializeField] Button rowingButton;
+
+        private int imageCounter = 0;
+
+        int currentCycle;
+
+        public void Initialize()
+        {
+            ShowButton();
+        }
+
+        public void SwitchImage()
+        {
+            if(imageCounter == 0)
+            {
+                first_1.enabled = false;
+                first_2.enabled = false;
+                second_1.enabled = true;
+                second_2.enabled = true;
+                imageCounter++;
+            }
+            else
+            {
+                Finish();
+            }
+            
+        }
+
+        void ShowButton()
+        {
+            rowingButton.gameObject.SetActive(true);
+        }
+
+        void Finish()
+        {
+            rowingButton.gameObject.SetActive(false);
+            MemoryManager.Instance.Next();
+        }
+    }
+}
