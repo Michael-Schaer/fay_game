8dd13bb864348b38c7b01073b69af0b46afda9a1
Markus Recher <markus.recher@fhnw.ch>
2019-11-09 17:23:34 +0100

    Intro Scene Added

---
+++ Assets/Scripts/ImageRotator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Fay
+{
+    public class ImageRotator : MonoBehaviour
+    {
+        [SerializeField] float rotationSpeed;
+        [SerializeField] float fallingSpeed;
+        [SerializeField] float delay;
+        [SerializeField] bool randomlySetValues;
+
+        bool fall;
+        
+        IEnumerator Start()
+        {
+            if (randomlySetValues)
+            {
+                rotationSpeed = Random.Range(-4f, 4f);
+                fallingSpeed = Random.Range(3f, 5f);
+                delay = Random.Range(6f, 10f);
+            }
+            
+            yield return new WaitForSeconds(delay);
+            fall = true;
+
+            yield return new WaitForSeconds(25);
+            
+        }
+
+        void Update()
+        {
+            if (fall)
+            {
+                transform.Rotate(Time.deltaTime * rotationSpeed * 100 * Vector3.forward);
+            
+                var position = transform.position;
+                position.y -= Time.deltaTime * fallingSpeed * 100;
+                transform.position = position;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/StartTitle.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class StartTitle : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI title;
+    
+    bool show;
+    
+    public void Show()
+    {
+        show = true;
+    }
+
+    float t = 0;
+    void Update()
+    {
+        if (show)
+        {
+            Color color = title.color;
+            color.a = Mathf.Lerp(color.a, 1, t);
+            title.color = color;
+            t = Mathf.Clamp01(t + 0.05f);
+        }
+    }
+}
