831bbcf086bcc18a48b1ecad003b6413401601e1
Markus Recher <markus.recher@fhnw.ch>
2019-11-09 11:17:42 +0100

    fingers script added

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DigitalRubyShared;
+using UnityEngine;
+
+public static class Extensions
+{
+    public static Vector2 TouchPosition(this GestureRecognizer gesture)
+    {
+        return new Vector2(gesture.FocusX, gesture.FocusY);
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using DigitalRubyShared;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    GestureRecognizer tapGesture;
+
+    void Start()
+    {
+        CreateTapGesture();
+    }
+
+    void CreateTapGesture()
+    {
+        tapGesture = new TapGestureRecognizer();
+        tapGesture.StateUpdated += OnTap;
+        FingersScript.Instance.AddGesture(tapGesture);
+    }
+
+    void OnTap(GestureRecognizer gesture)
+    {
+        if (gesture.State == GestureRecognizerState.Ended)
+        {
+           Debug.Log(Camera.main.ScreenToViewportPoint(gesture.TouchPosition()));
+            
+           
+        }
+    }
+}
